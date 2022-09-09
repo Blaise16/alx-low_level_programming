d2bb0607084c7655157faa29b32527d85c962cf4
Samuel Boateng <jaydenkyle15@gmail.com>
2022-09-09 00:30:23 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout
+ * Return: 1 if success
+ */
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19, \n")
+return (1);
+}

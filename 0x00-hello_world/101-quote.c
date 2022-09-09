da46fc86ee544b02267a63ddbb7450cffee75e33
Samuel Boateng <jaydenkyle15@gmail.com>
2022-09-09 01:31:14 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - prints the standard error sandwiched around the standard output
+ * main - prints the standard error sandwhiched around the standard output
  *
  * Return: returns 1
  */
 int main(void)
 {
-  write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",);
 return (1);
 }

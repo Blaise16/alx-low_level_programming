5d3c368fc2edf0cb19b8a6416c208234cf87c8c7
Samuel Boateng <jaydenkyle15@gmail.com>
2022-09-13 19:10:36 -0700

    prinys 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints the sum of 3 or
+ * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i = 0;
-	unsigned long int a = 0, b = 1, next =0;
+	unsigned long int a = 0, b = 1, next = 0;
 
 	while (i < 98)
 	{
@@ -18,9 +18,9 @@ int main(void)
 		printf("%lu", next);
 
 		if (i < 97)
-			printf(",");
+			printf(", ");
 		i++;
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }

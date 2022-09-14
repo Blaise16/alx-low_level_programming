aa395313366cda58787507eace7f3c44942403c3
Samuel Boateng <jaydenkyle15@gmail.com>
2022-09-13 18:45:56 -0700

    program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,6 @@
-* main - main block
+#include <stdio.h>
+/**
+ * main - main block
  * Description: computes and prints the sum of all the multiples of 3 or
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
@@ -10,15 +12,16 @@ int main(void)
 
 	while (i < 98)
 	{
-	next = a + b;
-	a = b;
-	b = next;
-	printf("%lu", next);
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
 
-	if (i < 97)
-	printf(", ");
-	i++;
+		if (i < 97)
+			printf(", ");
+		i++;
 	}
 	putchar('\n');
+
 	return (0);
 }
